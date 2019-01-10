a1ee64077483e7120f9d5965749843f3a82314f6
Julien <julien@nozav.org>
2019-01-10 15:38:22 +0100

    Add tests for simple clustering

--- tests/testthat/test_rainette1.R
+++ tests/testthat/test_rainette1.R
@@ -42,35 +42,72 @@ test_that("fchisq_val result is ok", {
 
 ## docs_order_by_ca
 
+indices <- rainette:::docs_order_by_ca(mini_dfm)
+
 test_that("doc_order_by_ca results are ok", {
   expect_equal(order(quanteda::textmodel_ca(mini_dfm)$rowcoord[,1]),
-               rainette:::docs_order_by_ca(mini_dfm))
+               indices)
 })
 
 ## split_tab_by_chisq
  
+tab <- mini_dfm %>% 
+  convert(to = "data.frame")
+tab <- tab[, -1] %>% 
+  t %>% 
+  as.data.frame
+res_split <- rainette:::split_tab_by_chisq(tab, indices)
+
 test_that("split_tab_by_chisq results are ok", {
-  indices <- rainette:::docs_order_by_ca(mini_dfm)
-  tab <- mini_dfm %>% 
-    convert(to = "data.frame")
-  tab <- tab[, -1] %>% 
-    t %>% 
-    as.data.frame
-  res <- rainette:::split_tab_by_chisq(tab, indices)
   manual_res <- data.frame(index = -1, chisq = -1)
   for (i in 2:5) {
     tab1 <- tab[, indices[1:i], drop = FALSE]
     tab2 <- tab[, indices[(i + 1):7, drop = FALSE]]
-    chisq <- chisq.test(cbind(rowSums(tab1), rowSums(tab2)))$statistic
+    expect_warning(chisq <- chisq.test(cbind(rowSums(tab1), rowSums(tab2)))$statistic)
     manual_res <- rbind(manual_res, c(index = indices[i], chisq = chisq))
   }
   max <- which.max(manual_res$chisq)
-  expect_equal(res$max_index, manual_res$index[max])
-  expect_equal(res$max_chisq, manual_res$chisq[max])
+  expect_equal(res_split$max_index, manual_res$index[max])
+  expect_equal(res_split$max_chisq, manual_res$chisq[max])
 })
 
+## switch_docs_by_chisq
 
-# rainette
+test_that("switch_docs_by_chisq results are ok", {
+  ## Wrong max_index et max_chisq
+  max_chisq <- 4.5
+  max_index <- 1
+  res_switch <- rainette:::switch_docs_by_chisq(tab, indices, max_index, max_chisq)
+  expect_equal(res_switch$indices1, c(3, 6))
+  expect_equal(res_switch$indices2, c(7, 4, 5, 2, 1))
+  expect_equal(res_switch$chisq, res_split$max_chisq)
+})
+
+## features_selection
+
+test_that("features_selection is ok", {
+  res_switch <- rainette:::switch_docs_by_chisq(tab, indices, res_split$max_index, res_split$max_chisq)
+  indices1 <- res_switch$indices1
+  indices2 <- res_switch$indices2
+  res <- rainette:::features_selection(tab, indices1, indices2)
+  expect_equal(res$cols1, character(0))
+  expect_equal(res$cols2, c("feat2", "feat3", "feat4"))
+  indices1 <- c(4, 3, 5, 6, 2)
+  indices2 <- c(7, 1)
+  res <- rainette:::features_selection(tab, indices1, indices2)
+  expect_equal(res$cols1, c("feat1", "feat3", "feat4"))
+  expect_equal(res$cols2, character(0))
+})
+
+## rainette
+
+test_that("rainette on mini_dfm is ok", {
+  res <- rainette(mini_dfm, k = 2, min_uc_size = 1, min_members = 1)
+  expect_is(res, c("rainette", "hclust"))
+  expect_equal(res$group, c(2, 2, 1, 2, 2, 1, 2))
+  expect_equal(which(res$group == 1), indices[1:which(indices == res_split$max_index)])
+  expect_equal(res$height, res_split$max_chisq)
+})
 
 mini_corpus <- head(data_corpus_inaugural, n = 2)
 mini_corpus <- split_segments(mini_corpus)
@@ -79,7 +116,7 @@ dtm <- dfm_wordstem(dtm, language = "english")
 dtm <- dfm_trim(dtm, min_termfreq = 3)
 
 test_that("warning if tab too small", {
-  expect_warning(rainette(dtm, k = 4, min_uc_size = 10, min_members = 1))
+  expect_warning(rainette(dtm, k = 8, min_uc_size = 10, min_members = 1))
 })
 
 test_that("warning if no more group > min_members", {
@@ -117,9 +154,17 @@ test_that("h argument generates error in cutree.rainette", {
   expect_error(cutree(res, h = 200))
 })
 
+## Special cases
 
-
-
+test_that("dfm with 'document' feature is ok", {
+  m_document <- m
+  colnames(m_document) <- c("document", "éé", "ùù", "feat4", "feat5")
+  dfm_document <- as.dfm(m_document)
+  res_document <- rainette(dfm_document, k = 2, min_uc_size = 1, min_members = 1)
+  res <- rainette(mini_dfm, k = 2, min_uc_size = 1, min_members = 1)
+  expect_equal(res$group, res_document$group)
+  expect_equal(res$height, res_document$height)  
+})
 
 
 
