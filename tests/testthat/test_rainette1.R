be91a9a1a4231aaf8978019c2b6cf7fe0cd1d951
Julien <julien@nozav.org>
2019-01-10 11:55:48 +0100

    Add tests for rainette functions

--- tests/testthat/test_rainette1.R
+++ tests/testthat/test_rainette1.R
@@ -16,6 +16,8 @@ m <- matrix(
 mini_dfm <- as.dfm(m)
 docvars(mini_dfm)$rainette_uce_id <- 1:nrow(mini_dfm)
 
+# compute_uc
+
 test_that("computed uc are ok", {
   dfm_uc <- rainette:::compute_uc(mini_dfm, min_uc_size = 3)
   expect_equal(docvars(dfm_uc)$rainette_uc_id, c(1,2,2,4,5,6,6))
@@ -24,6 +26,51 @@ test_that("computed uc are ok", {
 test_that("error if uc can't be computed", {
   expect_error(compute_uc(mini_dfm, min_uc_size = 5)) 
 })
+
+# fchisq_val
+
+test_that("fchisq_val result is ok", {
+  set.seed(13370)
+  t1 <- round(runif(100, 5, 50))
+  t2 <- round(runif(100, 10, 200))
+  row_sums <- t1 + t2
+  total <- sum(t1 + t2)
+  expect_equal(rainette:::fchisq_val(t1, t2, row_sums, total),
+               unname(chisq.test(cbind(t1, t2))$statistic))
+}) 
+
+
+## docs_order_by_ca
+
+test_that("doc_order_by_ca results are ok", {
+  expect_equal(order(quanteda::textmodel_ca(mini_dfm)$rowcoord[,1]),
+               rainette:::docs_order_by_ca(mini_dfm))
+})
+
+## split_tab_by_chisq
+ 
+test_that("split_tab_by_chisq results are ok", {
+  indices <- rainette:::docs_order_by_ca(mini_dfm)
+  tab <- mini_dfm %>% 
+    convert(to = "data.frame")
+  tab <- tab[, -1] %>% 
+    t %>% 
+    as.data.frame
+  res <- rainette:::split_tab_by_chisq(tab, indices)
+  manual_res <- data.frame(index = -1, chisq = -1)
+  for (i in 2:5) {
+    tab1 <- tab[, indices[1:i], drop = FALSE]
+    tab2 <- tab[, indices[(i + 1):7, drop = FALSE]]
+    chisq <- chisq.test(cbind(rowSums(tab1), rowSums(tab2)))$statistic
+    manual_res <- rbind(manual_res, c(index = indices[i], chisq = chisq))
+  }
+  max <- which.max(manual_res$chisq)
+  expect_equal(res$max_index, manual_res$index[max])
+  expect_equal(res$max_chisq, manual_res$chisq[max])
+})
+
+
+# rainette
 
 mini_corpus <- head(data_corpus_inaugural, n = 2)
 mini_corpus <- split_segments(mini_corpus)
@@ -59,6 +106,8 @@ test_that("groups correspondance is ok", {
   expect_equal(sum(tab > 0), 5)
 })
 
+## cutree
+
 test_that("generic cutree still works", {
   hc <- hclust(dist(USArrests))
   expect_length(cutree(hc, h = 250), 50)
@@ -67,6 +116,15 @@ test_that("generic cutree still works", {
 test_that("h argument generates error in cutree.rainette", {
   expect_error(cutree(res, h = 200))
 })
+
+
+
+
+
+
+
+
+
 
 
 
