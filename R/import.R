514f300607997bb3c0f0e70fd15e0863a1363dce
Julien <julien@nozav.org>
2019-01-14 21:19:23 +0100

    Add import_corpus_iramuteq function

---
+++ R/import.R
@@ -0,0 +1,135 @@
+## Extract texts from a list of documents in Iramuteq format
+## `thematics` indicate if potential thematics have to been removed
+
+extract_texts <- function(docs, thematics) {
+  purrr::map_chr(docs, function(text) {
+    ## Remove metadata
+    text <- stringr::str_replace_all(text, "^\\*\\w+_.*?\n", "")
+    ## Remove thematics
+    if (thematics == "remove") {
+      text <- stringr::str_replace_all(text, "(^|\n)-\\*[^ ].*?\n", "\\1")
+    }
+    text
+  }) 
+}
+
+## Extract metadata from a list of documents in Iramuteq format
+## and return them as a data.frame
+
+extract_metadata <- function(docs) {
+  meta <- purrr::map_dfr(docs, function(text) {
+    first_line <- stringr::str_extract(text, "^.*?\n")
+    vars_line <- stringr::str_extract_all(first_line, "\\*(.*?)[ \n$]")
+    
+    # No metadata for this doc
+    if(identical(vars_line[[1]], NA_character_)) {
+      # FIXME : doesn't work !
+      return(data.frame(rainette_nometa = NA))
+    }
+   
+     res <- purrr::map(vars_line, function(vars) {
+      vars <- stringr::str_replace(vars, "^\\*", "")
+      vars <- stringr::str_trim(vars)
+      vars <- stringr::str_split_fixed(vars, "_", n = 2)
+      values <- as.list(vars[,2])
+      names(values) <- vars[,1]
+      values
+    })
+    purrr::flatten_dfr(res)
+  })
+  meta[["rainette_nometa"]] <- NULL
+  meta
+}
+
+
+#' Import a corpus in Iramuteq format
+#'
+#' @param f a file name or a connection
+#' @param id_var name of metadata variable to be used as documents id
+#' @param thematics if "remove", thematics lines are removed. If "split", texts as splitted at each thematic, and metadata duplicated accordingly
+#' @param ... arguments passed to `file` if `f` is a file name.
+#'
+#' @details 
+#' A description of the Iramuteq corpus format can be found here : [http://www.iramuteq.org/documentation/html/2-2-2-les-regles-de-formatages](http://www.iramuteq.org/documentation/html/2-2-2-les-regles-de-formatages)
+#' 
+#' @return
+#' A quanteda corpus object. Note that metadata variables in docvars are all imported as characters.
+#' 
+#' @export
+#' @import stringr
+#' @importFrom rlang sym
+
+import_corpus_iramuteq <- function(f, id_var = NULL, thematics = c("remove", "split"), ...) {
+  
+  thematics <- match.arg(thematics)
+
+  ## Open as a filename or a connection
+  if (is.character(f)) {
+    f <- file(f, ...)
+  }
+  if (!inherits(f, "connection")) {
+    stop("f must be either a connection or a file path")
+  }
+  
+  ## Read text
+  lines <- readLines(f)
+  lines <- lines[lines != ""]
+  text <- paste(lines, collapse = "\n")
+
+  ## Detect if corpus has metadata
+  has_metadata <- stringr::str_detect(text, "(^|\n)\\*\\*\\*\\* +\\*\\w+_")
+  ## Detect if corpus has thematics
+  has_thematic <- stringr::str_detect(text, "(^|\n)-\\*[^ ]")
+  
+  ## Split into documents
+  docs <- stringr::str_split(text, "(^|\n)\\*\\*\\*\\*(\n| +)", simplify = TRUE)
+  docs <- docs[docs != ""]
+  
+  ## Extract texts
+  texts <- extract_texts(docs, thematics)
+  ## Extract metadata
+  if (has_metadata) {
+    metadata <- extract_metadata(docs)
+  }
+  
+  ## Split by thematics
+  if (has_thematic && thematics == "split") {
+    thems <- purrr::map(stringr::str_match_all(docs, "(^|\n)-\\*(.+)\n"), ~.x[,3])
+    thems <- purrr::flatten_chr(thems)
+    texts <- stringr::str_split(texts, "(^|\n)-\\*(.+)\n")
+    texts <- purrr::imap_dfr(texts, function(text, i) {
+      data.frame(id = i, text = text[text != ""], stringsAsFactors = FALSE)
+    })
+    ## Duplicate metadata and add thematics variable
+    if (has_metadata) {
+      metadata <- metadata[texts$id,]
+      metadata <- data.frame(metadata, thematics = thems, stringsAsFactors = FALSE)
+      ## Generate id by thematic if necessary
+      if (!is.null(id_var)) {
+        id_var_tidy <- rlang::sym(id_var)
+        metadata <- metadata %>% 
+          dplyr::group_by(!!id_var_tidy) %>% 
+          dplyr::mutate(rainette_split_id = paste(!!id_var_tidy, 1:n(), sep="_"))
+      }
+      id_var <- "rainette_split_id"
+    } else {
+      metadata <- data.frame(thematics = thems, stringsAsFactors = FALSE)
+      has_metadata <- TRUE
+    }
+    texts <- texts$text
+  }
+
+  ## Build corpus
+  corpus <- data.frame(text = texts, stringsAsFactors = FALSE)
+  if (has_metadata) {
+    corpus <- data.frame(metadata, corpus, check.names = FALSE)
+  }
+  if (!is.null(id_var)) {
+    rownames(corpus) <- corpus[[id_var]]
+    ## Remove computed id by thematic if present
+    corpus[["rainette_split_id"]] <- NULL
+  }
+  
+  quanteda::corpus(corpus)
+  
+}
---
+++ tests/testthat/test_import.R
@@ -0,0 +1,119 @@
+context("import Iramuteq corpus")
+
+text <- "**** *id_01 *sexe_m *age_18 *prof_ouvrier
+-*t1
+Donec placerat quis quam ac interdum. Cras tempus eros et laoreet dictum. Morbi ac dapibus magna.
+-*t2
+Vivamus dictum, nisi et suscipit fermentum, tortor nulla tempor quam, a ullamcorper elit erat nec arcu.
+**** *id_02 *sexe_f  *prof_agent_dentretien   *age_
+-*t1
+Vestibulum vitae ante eget justo maximus luctus. Nulla facilisi. 
+
+Integer tincidunt sed sapien mattis euismod. Proin sed massa neque. 
+-*t2
+Nam nunc neque, fringilla vel pharetra sit amet, posuere a nibh. 
+
+**** *id_03 *sexe_f *age_26
+-*t3
+Maecenas semper dictum nisi. 
+
+Sed dolor urna, fringilla sed justo vel, fermentum dictum magna. 
+
+-*t4
+Vestibulum mollis, ligula in semper vulputate, mauris diam aliquam est, ut placerat sem felis a nibh."
+
+con <- textConnection(text)
+corpus <- import_corpus_iramuteq(con, id_var = "id", thematics = "remove")
+close(con)
+con <- textConnection(text)
+corpus_split_id <- import_corpus_iramuteq(con, id_var = "id", thematics = "split")
+close(con)
+con <- textConnection(text)
+corpus_split <- import_corpus_iramuteq(con, thematics = "split")
+close(con)
+
+test_that("metadata imported correctly", {
+  expect_equal(docvars(corpus)$id, c("01", "02", "03"))
+  expect_equal(docvars(corpus)$sexe, c("m", "f", "f"))
+  expect_equal(docvars(corpus)$age, c("18", "", "26"))
+  expect_equal(docvars(corpus)$prof, c("ouvrier", "agent_dentretien", NA))
+  expect_equal(docnames(corpus), docvars(corpus)$id)
+})
+
+test_that("texts imported correctly", {
+  expect_equal(ndoc(corpus), 3)
+  expect_equal(texts(corpus)[["02"]], "Vestibulum vitae ante eget justo maximus luctus. Nulla facilisi. \nInteger tincidunt sed sapien mattis euismod. Proin sed massa neque. \nNam nunc neque, fringilla vel pharetra sit amet, posuere a nibh. ")
+})
+
+
+test_that("thematics splitted correctly", {
+  expect_equal(ndoc(corpus_split), 6)
+  expect_equal(ndoc(corpus_split_id), 6)
+  expect_equal(texts(corpus_split_id)[["02_2"]], "Nam nunc neque, fringilla vel pharetra sit amet, posuere a nibh. ")
+  expect_equal(docnames(corpus_split), paste0("text", 1:6))
+  expect_equal(docnames(corpus_split_id), c("01_1", "01_2", "02_1", "02_2", "03_1", "03_2"))
+  expect_equal(names(docvars(corpus_split)), names(docvars(corpus_split_id)))
+  expect_equal(docvars(corpus_split)$age, c("18", "18", "", "", "26", "26"))
+  expect_equal(docvars(corpus_split)$age, docvars(corpus_split_id)$age)
+  expect_equal(docvars(corpus_split)$thematic, docvars(corpus_split_id)$thematic)
+  expect_equal(docvars(corpus_split)$thematic, c("t1", "t2", "t1", "t2", "t3", "t4"))
+})
+
+text <- "****
+-*t1
+Donec placerat quis quam ac interdum.
+-*t2
+Vivamus dictum, nisi et suscipit fermentum, tortor nulla tempor quam, a ullamcorper elit erat nec arcu.
+****
+-*t1
+Vestibulum vitae ante eget justo maximus luctus. 
+-*t2
+Nam nunc neque, fringilla vel pharetra sit amet, posuere a nibh."
+
+con <- textConnection(text)
+corpus_without_meta <- import_corpus_iramuteq(con, id_var = "id", thematics = "remove")
+close(con)
+con <- textConnection(text)
+corpus_without_meta_split <- import_corpus_iramuteq(con, id_var = "id", thematics = "split")
+close(con)
+
+
+test_that("corpus without meta is ok", {
+  expect_equal(ndoc(corpus_without_meta), 2)
+  expect_equal(texts(corpus_without_meta)[[2]], c("Vestibulum vitae ante eget justo maximus luctus. \nNam nunc neque, fringilla vel pharetra sit amet, posuere a nibh."))
+  expect_equal(dim(docvars(corpus_without_meta)), c(2, 0))
+})
+
+test_that("splitted corpus without meta is ok", {
+  expect_equal(dim(docvars(corpus_without_meta_split)), c(4, 1))
+  expect_equal(docvars(corpus_without_meta_split)$thematics, c("t1","t2","t1","t2"))
+  expect_equal(ndoc(corpus_without_meta_split), 4)
+  expect_equal(texts(corpus_without_meta_split)[[3]], "Vestibulum vitae ante eget justo maximus luctus. ")
+})
+
+text <- "****
+Donec placerat quis quam ac interdum.
+**** *foo_1
+Vestibulum vitae ante eget justo maximus luctus.
+****
+Donec placerat quis quam ac interdum.
+**** *bar_bar
+Vestibulum vitae ante eget justo maximus luctus."
+
+con <- textConnection(text)
+corpus_with_partial_meta <- import_corpus_iramuteq(con, id_var = "id", thematics = "remove")
+close(con)
+
+test_that("corpus with partial meta is ok", {
+  expect_equal(texts(corpus_with_partial_meta)[[2]], "Vestibulum vitae ante eget justo maximus luctus.")
+  expect_equal(ndoc(corpus_with_partial_meta), 4)
+  expect_equal(dim(docvars(corpus_with_partial_meta)), c(4, 2))
+  expect_equal(docvars(corpus_with_partial_meta)$foo, c(NA, "1", NA, NA))
+  expect_equal(docvars(corpus_with_partial_meta)$bar, c(NA, NA, NA, "bar"))
+})
+
+test_that("error if f is not a valid file path", {
+  expect_error(expect_warning(import_corpus_iramuteq("doesnt/exist.txtt")))
+  expect_error(import_corpus_iramuteq(mtcars))
+})
+
