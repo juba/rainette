b3b59cb8b1dacc79c08311ab12a4ae85be8427fa
Julien <julien@nozav.org>
2019-01-24 11:37:55 +0100

    Update tests

--- tests/testthat/test_cutree.R
+++ tests/testthat/test_cutree.R
@@ -17,7 +17,7 @@ test_that("generic cutree still works", {
 
 test_that("cutree_rainette is ok", {
   expect_error(cutree(res, h = 200))
-  expect_equal(cutree(res, k = 3), c(3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 2L, 
-    2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 3L, 3L, 3L, 3L, 1L, 1L, 1L, 
-    3L, 3L, 3L, 3L, 3L, 2L, 2L, 2L, 2L, 2L, 2L, 2L))
+  expect_equal(cutree(res, k = 3), c(3L, 3L, 3L, 3L, 3L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 2L, 2L, 
+    2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 
+    2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 1L))
 })
--- tests/testthat/test_split_segments.R
+++ tests/testthat/test_split_segments.R
@@ -1,5 +1,6 @@
 library(quanteda)
 library(tm)
+library(tibble)
 context("split_segments")
 
 data(acq)
@@ -14,16 +15,38 @@ split_quanteda <- split_segments(corpus)
 test_that("split_segments.character is ok", {
   expect_equal(dim(split_char), c(40,1))
   expect_equal(as.character(split_char[1,]), "Fellow-Citizens of the Senate and of the House of Representatives : Among the vicissitudes incident to life no event could have filled me with greater anxieties than that of which the notification was transmitted by your order , and received on the 14th day of the present month .")
+  expect_equal(split_segments("One more test."), tibble(segment = "One more test."))
+  expect_equal(split_segments("J'apprends des techniques d'écriture. Cela me permet d'écrire autre choses que des lettres ou un récit de ma vie/journal_intime. Cela donne des idées pour écrire des fictions, sortir de son quotidien. J'aime bien l'effort intellectuel que cela demande. C'est un rendez-vous apaisant."), tibble(segment = "J'apprends des techniques d'écriture . Cela me permet d'écrire autre choses que des lettres ou un récit de ma vie / journal_intime . Cela donne des idées pour écrire des fictions , sortir de son quotidien . J'aime bien l'effort intellectuel que cela demande . C'est un rendez-vous apaisant ."))
+  
+  expect_equal(split_segments("Cool ! Cool cool cool."), 
+    tibble(segment = c("Cool ! Cool cool cool.")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 1), 
+    tibble(segment = c("Cool", "!", "Cool", "cool", "cool", ".")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 1, 1), 
+    tibble(segment = c("Cool !", "Cool", "cool", "cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 1, 2), 
+    tibble(segment = c("Cool !", "Cool", "cool cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 2), 
+    tibble(segment = c("Cool !", "Cool cool", "cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 2, 2), 
+    tibble(segment = c("Cool !", "Cool cool cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 3), 
+    tibble(segment = c("Cool !", "Cool cool cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 3, 2), 
+    tibble(segment = c("Cool !", "Cool cool cool .")))
+  expect_equal(split_segments("Cool ! Cool cool cool.", 3, 3), 
+    tibble(segment = c("Cool ! Cool cool cool .")))
+  
 }) 
 
 test_that("split_segments.Corpus is ok", {
-  expect_equal(ndoc(split_tm), 183)
+  expect_equal(ndoc(split_tm), 224)
   expect_equal(docvars(split_tm)$segment_source[1], "10")
   expect_equal(rownames(docvars(split_tm))[1], "10_1")
 })
 
 test_that("split_segments.corpus is ok", {
-  expect_equal(ndoc(split_quanteda), 248)
+  expect_equal(ndoc(split_quanteda), 253)
   expect_equal(docvars(split_quanteda)$segment_source[1], "1789-Washington")
   expect_equal(rownames(docvars(split_quanteda))[1], "1789-Washington_1")
 })
