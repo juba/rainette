9098ce0026e8170f14e3aa65725261734665105a
Julien <julien@nozav.org>
2020-02-12 11:35:05 +0100

    Fix uce groups test

--- tests/testthat/test_rainette1.R
+++ tests/testthat/test_rainette1.R
@@ -108,8 +108,10 @@ test_that("Stopping if no more group > min_members", {
 })
 
 test_that("all uce belong to the same group", {
-  res <- rainette(dtm, k = 3, min_uc_size = 15)
-  expect_equal(anyDuplicated(unique(cbind(res$corresp_uce_uc$uc, res$group))), 0)
+  res <- rainette(dtm, k = 3, min_uc_size = 5)
+  dtm_uc <- rainette:::compute_uc(dtm, min_uc_size = 5)
+  expect_equal(anyDuplicated(unique(cbind(docvars(dtm_uc)$rainette_uc_id, res$group))), 0)
+  expect_equal(anyDuplicated(unique(cbind(docvars(dtm_uc)$rainette_uce_id, res$group))), 0)
 })
 
 corpus <- head(data_corpus_inaugural, n = 10)
