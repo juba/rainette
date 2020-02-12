e887e346c56d14dc39d9ec7962b51f3695732648
Julien <julien@nozav.org>
2020-02-12 10:51:48 +0100

    Fix cutree tests

--- tests/testthat/test_cutree.R
+++ tests/testthat/test_cutree.R
@@ -6,7 +6,7 @@ mini_corpus <- split_segments(mini_corpus)
 dtm <- dfm(mini_corpus, remove = stopwords("en"), tolower = TRUE, remove_punct = TRUE)
 dtm <- dfm_wordstem(dtm, language = "english")
 dtm <- dfm_trim(dtm, min_termfreq = 3)
-res <- rainette(dtm, k = 3, min_uc_size = 15)
+res <- rainette(dtm, k = 3, min_uc_size = 10)
 
 ## cutree
 
@@ -17,9 +17,9 @@ test_that("generic cutree still works", {
 
 test_that("cutree_rainette is ok", {
   expect_error(cutree(res, h = 200))
-  expect_equal(cutree(res, k = 3), c(3L, 3L, 3L, 3L, 3L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 2L, 2L, 
-    2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 
-    2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 1L))
+  expect_equal(cutree(res, k = 3), c(3L, 3L, 3L, 3L, 3L, 3L, 3L, 2L, 2L, 2L, 2L, 3L, 3L, 2L, 2L, 
+    2L, 2L, 2L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 2L, 2L, 
+    2L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 1L, 1L))
 })
 
 test_that("rainette2_complete_groups", {
