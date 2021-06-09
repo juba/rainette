382c16d17bcadef582c92ad1e3356a0b03c958fd
Julien <julien@nozav.org>
2021-06-09 16:12:34 +0200

    Don't merge uce from different documents.

--- R/utils.R
+++ R/utils.R
@@ -1,58 +1,77 @@
-if (getRversion() >= "2.15.1")  
-  utils::globalVariables(c("Dim1", "distance", "index", "segment", "segment_source", "weight", "p", 
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(c("Dim1", "distance", "index", "segment", "segment_source", "weight", "p",
     "chi2", "n_both", "n1", "n2", "g1", "g2", "level1", "level2", "members", "clusters", "feature",
     "Group", "rainette_group", "text"))
 
 
 #' Merges uces into uc according to minimum uc size
-#' 
+#'
 #' `rainette_uc_index` docvar
-#' 
+#'
 #' @param dtm dtm of uces, with a `rainette_uce_id` docvar
 #' @param min_uc_size minimum number of forms by uc
+#' @param doc_id character name of a dtm docvar which identifies source documents.
 #'
 #' @details
-#' Internal function, not to be used directly
+#' If `min_uc_size == 0`, different uc ids are added to the dtm docvars
+#' (ie no uce are merged together). If `min_uc_size > 0` then `doc_id` must be provided
+#' unless the corpus comes from `split_segments`, in this case
+#' `segment_source` is used by default.
 #'
 #' @return the original dtm with a new `rainette_uc_id` docvar.
 #' @export
 
-compute_uc <- function(dtm, min_uc_size = 10) {
-  
+compute_uc <- function(dtm, min_uc_size = 10, doc_id = NULL) {
+
   ## Add id to documents
   quanteda::docvars(dtm, field = "rainette_uce_id") <- seq_len(nrow(dtm))
-  
+
   if (min_uc_size <= 1) {
     ## Do nothing
     quanteda::docvars(dtm, field = "rainette_uc_id") <- quanteda::docvars(dtm, "rainette_uce_id")
     return(dtm)
   }
-  
+
+  ## Check for min_uc_size and doc_id values
+  if (is.null(doc_id)) {
+    if ("segment_source" %in% names(docvars(dtm))) doc_id <- "segment_source"
+    else stop("If min_uc_size > 0, you must provide a doc_id value.")
+  }
+
   ## Size of each uce
   terms_by_uce <- rowSums(dtm)
+  doc_ids <- quanteda::docvars(dtm, doc_id)
   if (any(terms_by_uce < min_uc_size)) {
+    uc_size_warning <- FALSE
     index <- 1
     uc_id <- quanteda::docvars(dtm, "rainette_uce_id")
     while (index < length(terms_by_uce)) {
       current_size <- terms_by_uce[index]
       grouping_index <- index
       ## While current uc size is smaller than min, regroup with following uce
-      while(current_size < min_uc_size) {
-        grouping_index <- grouping_index + 1
-        if (grouping_index > length(terms_by_uce)) {
-          stop("can't compute uc with respect to min_uc_size")
+      while (current_size < min_uc_size) {
+        if ((grouping_index + 1) <= length(terms_by_uce) && 
+              doc_ids[grouping_index] == doc_ids[grouping_index + 1]) {
+          grouping_index <- grouping_index + 1
+          current_size <- current_size + terms_by_uce[grouping_index]
+          uc_id[grouping_index] <- index
+        } else {
+          ## If new index is out of bounds or in another document
+          uc_size_warning <- TRUE
+          break
         }
-        current_size <- current_size + terms_by_uce[grouping_index]
-        uc_id[grouping_index] <- index
       }
       index <- grouping_index + 1
+    }
+    if (uc_size_warning) {
+      warning("some uc will have a size < min_uc_size")
     }
     ## Add computed uc ids to docvars
     quanteda::docvars(dtm, "rainette_uc_id") <- uc_id
   } else {
     quanteda::docvars(dtm, "rainette_uc_id") <- quanteda::docvars(dtm, "rainette_uce_id")
   }
-  
+
   return(dtm)
 }
 
@@ -61,12 +80,10 @@ compute_uc <- function(dtm, min_uc_size = 10) {
 
 stat_col <- function(measure) {
 
-    stat_col <- switch(measure,
+  stat_col <- switch(measure,
     "chi2" = "chi2",
     "lr" = "G2"
   )
   stat_col <- rlang::sym(stat_col)
-  
+
 }
-
-
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -2,30 +2,60 @@ library(quanteda)
 context("utils functions")
 
 m <- matrix(
-  c(1,1,1,0,0,
-    0,0,1,1,0,
-    1,0,0,0,0,
-    1,1,1,1,0,
-    1,1,1,1,1,
-    1,0,0,0,0,
-    1,1,1,0,0
+  c(1, 1, 1, 0, 0,
+    0, 0, 1, 1, 0,
+    1, 0, 0, 0, 0,
+    1, 1, 1, 1, 0,
+    1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0,
+    1, 1, 1, 0, 0
     ),
   ncol = 5,
   byrow = TRUE,
 )
 mini_dfm <- as.dfm(m)
-docvars(mini_dfm, "rainette_uce_id") <- 1:nrow(mini_dfm)
+docvars(mini_dfm, "rainette_uce_id") <- seq_len(nrow(mini_dfm))
+docvars(mini_dfm, "doc_id") <- "doc1"
+
 
 # compute_uc
 
-test_that("computed uc are ok", {
-  dfm_uc <- rainette:::compute_uc(mini_dfm, min_uc_size = 3)
-  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1,2,2,4,5,6,6))
+test_that("computed uc are ok when min_uc_size == 0", {
+  dfm_uc <- rainette::compute_uc(mini_dfm, min_uc_size = 0)
+  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1, 2, 3, 4, 5, 6, 7))
 })
 
-test_that("error if uc can't be computed", {
-  expect_error(compute_uc(mini_dfm, min_uc_size = 5)) 
+test_that("error when min_uc_size > 0 and no doc_id", {
+  expect_error(compute_uc(mini_dfm, min_uc_size = 3))
 })
+
+test_that("computed uc are ok when min_uc_size > 0", {
+  dfm_uc <- rainette::compute_uc(mini_dfm, min_uc_size = 3, doc_id = "doc_id")
+  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1, 2, 2, 4, 5, 6, 6))
+})
+
+test_that("warning if uc can't be computed", {
+  expect_warning(compute_uc(mini_dfm, min_uc_size = 5, doc_id = "doc_id"))
+})
+
+mini_dfm_docs <- mini_dfm
+test_that("computed uc are ok with respect to doc_id", {
+  docvars(mini_dfm_docs, "doc_id") <- c("doc1", "doc1", "doc1", "doc2", "doc2", "doc2", "doc3")
+  expect_warning(dfm_uc <- rainette::compute_uc(mini_dfm_docs, min_uc_size = 3, doc_id = "doc_id"))
+  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1, 2, 2, 4, 5, 6, 7))
+
+  docvars(mini_dfm_docs, "doc_id") <- c("doc1", "doc2", "doc3", "doc4", "doc4", "doc5", "doc5")
+  expect_warning(dfm_uc <- rainette::compute_uc(mini_dfm_docs, min_uc_size = 3, doc_id = "doc_id"))
+  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1, 2, 3, 4, 5, 6, 6))
+})
+
+mini_dfm_segsource <- mini_dfm
+docvars(mini_dfm_segsource, "segment_source") <- "doc1"
+test_that("segment_source used by default when min_uc_size > 0", {
+  dfm_uc <- rainette::compute_uc(mini_dfm_segsource, min_uc_size = 3)
+  expect_equal(docvars(dfm_uc, "rainette_uc_id"), c(1, 2, 2, 4, 5, 6, 6))
+})
+
 
 # stat_col
 
@@ -33,5 +63,3 @@ test_that("stat_col is ok", {
   expect_equal(stat_col("chi2"), rlang::sym("chi2"))
   expect_equal(stat_col("lr"), rlang::sym("G2"))
 })
-
-
