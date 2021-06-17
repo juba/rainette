2f57b562c15f729d90b9a956bd5d50ac113d3072
Julien <julien@nozav.org>
2021-06-17 12:44:50 +0200

    guide = FALSE -> guide = "none"

--- R/plot.R
+++ R/plot.R
@@ -15,7 +15,7 @@ keyness_barplot <- function(tab, range = NULL, title = "", title_color = "firebr
     geom_col(aes(fill = sign), color = "white", width = 1) +
     geom_text(y = stat_max / 15, aes(label = stats::reorder(feature, !!stat_col_tidy)), hjust = 0, size = text_size / 2.5) +
     coord_flip() +
-    scale_fill_manual("", guide = FALSE,
+    scale_fill_manual("", guide = "none",
       values = c("positive" = "#a1d8ff", "negative" = "#ff7d7e")) +
     labs(title = title, x = "") +
     theme_minimal() +
@@ -318,7 +318,7 @@ rainette2_plot <- function(res, dtm, k = NULL, criterion = c("chi2", "n"),
   colnames(freq) <- c("Group", "n")
   g <- ggplot(freq) +
     geom_col(aes(x = Group, y = n, fill = Group)) +
-    scale_fill_manual(guide = FALSE, values = c(groups_colors(k)), na.value = "grey20") +
+    scale_fill_manual(guide = "none", values = c(groups_colors(k)), na.value = "grey20") +
     ggtitle(title) +
     theme(
       plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
