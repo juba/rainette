f735344882f16cdb6892246cdd0915baa030d076
Julien Barnier <julien.barnier@cnrs.fr>
2022-02-11 17:30:17 +0100

    Fix rlang::.env import

--- R/cutree.R
+++ R/cutree.R
@@ -55,13 +55,13 @@ cutree_rainette <- function(hres, k = NULL, h = NULL,...) {
 #' A vector with group membership.
 #'
 #' @seealso [rainette2_complete_groups()]
-#'
+#' @importFrom rlang .env
 #' @export
 
 cutree_rainette2 <- function(res, k, criterion = c("chi2", "n"), ...) {
   criterion <- match.arg(criterion)
   line <- res %>%
-    dplyr::filter(k == rlang::.env$k)
+    dplyr::filter(k == .env$k)
   if (criterion == "chi2") {
     line <- line %>%
       dplyr::slice_max(chi2)
